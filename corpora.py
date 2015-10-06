fefe5eceab39e76d649a3e8e7b654689a067c9b6
Roger Wechsler <mr.mutator@gmail.com>
2015-10-06 14:45:40 +0200

    Updates.

--- corpora.py
+++ corpora.py
@@ -11,7 +11,8 @@ def get_swda_utterances(swda_dir):
     for trans in  corpus.iter_transcripts(display_progress=True):
             for utt in trans.utterances:
                 utt_tokens =  word_tokenize(re.sub(r'\{.+? |\}|\[|\]|\+|#|/|<.+?>', "", utt.text.lower()))
-                yield utt.damsl_act_tag(), utt_tokens
+                if utt_tokens:
+                    yield utt.damsl_act_tag(), utt_tokens
 
 def get_SB_utterances(SB_dir):
     for f in glob.glob(SB_dir + "/*.trn"):
@@ -36,7 +37,28 @@ def get_SB_utterances(SB_dir):
 
         infile.close()
 
+def write_swda_file(swda_dir, outfile_name):
+    outfile = codecs.open(outfile_name, "w", "utf-8")
+    for tag, utt_tokens in get_swda_utterances(swda_dir):
+        outfile.write(tag + "\t" + " ".join(utt_tokens) + "\n")
+    outfile.close()
+
+def get_swda_utterances_from_file(swda_file):
+    infile = codecs.open(swda_file, "r", "utf-8")
+    for line in infile:
+        try:
+            tag, utt = line.strip().split("\t")
+        except ValueError:
+            print "---->>>>>>", line.strip(), line.split("\t")
+            break
+        yield tag, utt.split()
+
+
 
 if __name__ == "__main__":
-    for tag, utt in get_SB_utterances("data/SB"):
-        print tag, utt
+    # for tag, utt in get_SB_utterances("data/SB"):
+        # print tag, utt
+    write_swda_file("data/swda", "data/swda_file.txt")
+
+    for tag, tokens in get_swda_utterances_from_file("data/swda_file.txt"):
+        print tag, tokens
