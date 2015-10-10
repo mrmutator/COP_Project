84bce4b9c85a33c26aedda4e5f26fc982a6bcf09
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2015-10-10 19:41:21 +0200

    classifiers added

--- classifier.py
+++ classifier.py
@@ -1,41 +1,89 @@
 __author__ = 'rwechsler'
 from sklearn import svm
 from sklearn import naive_bayes
-from corpora import get_swda_utterances_from_file
+from corpora import get_utterances_from_file
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import preprocessing
 from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 
-X_tokens = []
-Y_tags = []
-for tag, tokens in get_swda_utterances_from_file("data/swda_file.txt"):
-    X_tokens.append(" ".join(tokens))
-    # remove id from tag
-    tag = tag.split("/")[0]
-    Y_tags.append(tag)
+def NB_classifier(X_train, Y_train, X_test, Y_test):
+    model = naive_bayes.BernoulliNB()
+    model.fit(X_train, Y_train)
+    score = model.score(X_test, Y_test)
 
-vectorizer = CountVectorizer(min_df=1)
-X = vectorizer.fit_transform(X_tokens)
-
-le = preprocessing.LabelEncoder()
-le.fit(Y_tags)
-Y = le.transform(Y_tags)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
+    return score
 
 
-#model = svm.SVC()
-model = naive_bayes.BernoulliNB()
+def SVM_classifier(X_train, Y_train, X_test, Y_test):
+    SVM_model = svm.SVC(probability=False, kernel='rbf', C=1.0)
+    SVM_model.fit(X_train,Y_train)
+    score = SVM_model.score(X_test, Y_test)
 
-model.fit(X_train, Y_train)
-acc = model.score(X_train, Y_train)
-print "Training accuracy", acc
-
-acc = model.score(X_test, Y_test)
-print "Test accuracy", acc
+    return score
 
 
+def KNN_classifier(X_train, Y_train, X_test, Y_test):
+    KNN_model = KNeighborsClassifier(n_neighbors=5)
+    KNN_model.fit(X_train,Y_train)
+    score = KNN_model.score(X_test,Y_test)
+
+    return score
+
+
+def get_swda_labeled_utterances():
+    X_tokens = []
+    Y_tags = []
+    for tag, tokens in get_utterances_from_file("data/swda_file.txt"):
+        X_tokens.append(" ".join(tokens))
+        # remove id from tag
+        tag = tag.split("/")[0]
+        Y_tags.append(tag)
+
+    return X_tokens, Y_tags
+
+
+def get_BOW_from_utterances(X_tokens):
+    # scikit learn BOW
+    vectorizer = CountVectorizer(min_df=1)
+    X = vectorizer.fit_transform(X_tokens) # returns a sparse matrix
+
+    return X
+
+
+def encode_tags(tags):
+    # encode tags to ints
+    le = preprocessing.LabelEncoder()
+    le.fit(tags) # fit data
+    Y = le.transform(tags) # return normalized tags
+
+    return Y
+
+
+def split_datasets(X, Y):
+    # out of the box scikit method for splitting train/test. Get 10%
+    return train_test_split(X, Y, test_size=0.1)
+
+
+if __name__=='__main__':
+
+    # get utterance tokens and tags from swda corpus
+    X_tokens, Y_tags = get_swda_labeled_utterances()
+
+    # get BOW for utterances
+    X = get_BOW_from_utterances(X_tokens)
+
+    # encode tags as ints
+    Y = encode_tags(Y_tags)
+
+    # get training and validation sets
+    X_train, X_test, Y_train, Y_test = split_datasets(X,Y)
+
+    # run classifiers
+    NB_score = NB_classifier(X_train, Y_train, X_test, Y_test)
+    # SVM_score = SVM_classifier(X_train, Y_train, X_test, Y_test)
+    KNN_score = KNN_classifier(X_train, Y_train, X_test, Y_test)
 
 # predicted =  model.predict(X_test)
 # true = le.inverse_transform(Y_test)
